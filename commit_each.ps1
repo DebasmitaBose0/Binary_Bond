208b057ca24ccd11ff05f1f691e0417538dc1ee4
KGFCH2 <babinbid05@gmail.com>
2026-01-01 06:19:49 +0530

    Deleted

--- commit_each.ps1
+++
@@ -1,9 +0,0 @@
-$files = @()
-$files += git diff --name-only
-$files += git ls-files --others --exclude-standard
-foreach ($f in $files) {
-    if ($f -and $f.Trim() -ne "") {
-        git add -- "$f"
-        git commit -m "chore: update $f"
-    }
-}
